494adf3ac09ee468b0a0e81f4c580c10efcfb710
cyclexit <cyclexit@gmail.com>
2019-12-19 21:41:48 -0500

    Delete useless comments

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -2,13 +2,13 @@
 // This class can be used for both directed and undirected graph
 class Graph {
  public:
-  int n; // number of nodes
-  vector<vector<int>> edge; // edges
+  int n;
+  vector<vector<int>> edge;
   // constructor
   Graph(int _n) : n(_n) {
     edge.resize(n);
   }
-  // function
+  // add an edge
   void add(int u, int v) {
     edge[u].push_back(v);
   }
