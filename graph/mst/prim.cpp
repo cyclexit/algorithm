02994b67443c2d914ea903888fa877352d23fe74
cyclexit <cyclexit@gmail.com>
2020-02-18 10:38:16 -0500

    Make it more readable

--- graph/mst/prim.cpp
+++ graph/mst/prim.cpp
@@ -1,29 +1,30 @@
-// This class is used for unweighted graph
-// This class can be used for both directed and undirected graph
+/*
+ * MST-Prim
+ * The complexity of the algorithm depends on the implementation of the 
+ * priority queue.
+ * Binary heap: O(Elog(V) + Vlog(V))
+ * Fibonacci heap: O(E + log(V))
+ */
 class WeightedGraph {
  public:
+  typedef long long ll;
+
   int n;
   // first: adjacent node
   // second: weight
-  vector<vector<pair<int, long long>>> edge;
-  // constructor
+  vector<vector<pair<int, ll>>> edge;
+
   WeightedGraph(int _n) : n(_n) {
     edge.resize(n);
   }
-  // add a weighted edge
-  void add(int u, int v, long long w) {
+
+  void add(int u, int v, ll w) {
     edge[u].emplace_back(make_pair(v, w));
   }
-  /*
-   * MST-Prim
-   * The complexity of the algorithm depends on the implementation of the 
-   * priority queue.
-   * Binary heap: O(Elog(V) + Vlog(V))
-   * Fibonacci heap: O(E + log(V))
-   */
+  
   // first: adjacent node
   // second: key(weight)
-  vector<pair<int, long long>> mst;
+  vector<pair<int, ll>> mst;
   void prim() {
     mst.resize(n);
     for (int i = 0; i < n; ++i) {
@@ -31,13 +32,13 @@ class WeightedGraph {
     }
     // first: key(weight)
     // second: node index
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
     // start from node 1
     pq.emplace(make_pair(0, 1));
     mst[1].second = 0;
     vector<bool> vis(n, false);
     while (!pq.empty()) {
-      pair<int, long long> cur = pq.top();
+      pair<int, ll> cur = pq.top();
       pq.pop();
       vis[cur.second] = true;
       for (auto x : edge[cur.second]) {
